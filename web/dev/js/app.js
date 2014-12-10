bc0df8244850dcb9dff2505fb3caf2ff1ffb2356
colorpeach <514571352@qq.com>
2014-12-10 17:15:55 +0800

    normal

--- web/dev/js/app.js
+++ web/dev/js/app.js
@@ -6,7 +6,6 @@ define(['angular-route', 'angular-animate', 'js/common'], function(){
         defaultRoutePath: '/',
         routes: {
             '/': {
-                full: true,
                 title: '桌面',
                 icon: 'icon-home',
                 templateUrl: '/index.html',
@@ -103,7 +102,6 @@ define(['angular-route', 'angular-animate', 'js/common'], function(){
                 ]
             },
             '/edit/app/:id': {
-                full: true,
                 title: '编辑应用',
                 auth: 'yes',
                 templateUrl: 'app-edit.html',
@@ -115,7 +113,6 @@ define(['angular-route', 'angular-animate', 'js/common'], function(){
                 ]
             },
             '/edit/app-pro/:id': {
-                full: true,
                 title: '编辑应用',
                 auth: 'yes',
                 templateUrl: 'app-edit-pro.html',
@@ -130,7 +127,6 @@ define(['angular-route', 'angular-animate', 'js/common'], function(){
                 ]
             },
             '/edit/snippet/:id': {
-                full: true,
                 title: '编辑代码片段',
                 auth: 'yes',
                 templateUrl: 'snippet-edit.html',
@@ -215,7 +211,6 @@ define(['angular-route', 'angular-animate', 'js/common'], function(){
 
                                 $rootScope.title = route.title;
                                 $rootScope.href = $location.path();
-                                $rootScope.full = !!route.full;
                                 $window.document.title = 'colorBox-' + route.title;
 
                                 $rootScope.$apply(function(){
@@ -286,21 +281,6 @@ define(['angular-route', 'angular-animate', 'js/common'], function(){
         }
     ]);
 
-    app.controller('indexCtrl',
-    ['$scope',
-        function($scope){
-            $scope.showAside = false;
-
-            $scope.toggleAside = function(){
-                $scope.showAside = !$scope.showAside;
-            };
-            
-            $scope.$on('$routeChangeSuccess', function(){
-                $scope.showAside = false;
-            });
-        }
-    ]);
-
     app
     .filter('desktopNavs', 
     [
@@ -321,7 +301,7 @@ define(['angular-route', 'angular-animate', 'js/common'], function(){
     .filter('logoNavs', 
     [
         function(){
-            var showTabs = ['/dashboard/appPros', '/snippet-square', '/dashboard/account', '/dashboard/apps', '/'];
+            var showTabs = ['/dashboard/appPros', '/snippet-square', '/message', '/'];
             return function(navs){
                 var r = [];
                 angular.forEach(navs, function(n, i){
@@ -355,6 +335,80 @@ define(['angular-route', 'angular-animate', 'js/common'], function(){
             }
         }
     ])
+
+    .directive('drag',
+    ['$window', '$timeout',
+        function($window, $timeout){
+            var eventsMap = {
+                web: {
+                    down: 'mousedown',
+                    up: 'mouseup',
+                    move: 'mousemove'
+                },
+                mobile: {
+                    down: 'touchstart',
+                    up: 'touchend',
+                    move: 'touchmove'
+                }
+            };
+            var device;
+            var $body = angular.element($window.document.body);
+
+            if($window.document.hasOwnProperty("ontouchstart")){
+                device = 'mobile';
+            }else{
+                device = 'web';
+            }
+            return {
+                restrict: 'A',
+                link: function(scope, element, attrs){
+                    element.bind(eventsMap[device].down, startDrag);
+
+                    function startDrag(e){
+                        var rect = element[0].getBoundingClientRect();
+                        var position = {};
+                        var relative = {};
+                        var point = {};
+
+                        if(e.touches){
+                            relative.x = e.touches[0].clientX - rect.left;
+                            relative.y = e.touches[0].clientY - rect.top;
+                        }else{
+                            relative.x = e.clientX - rect.left;
+                            relative.y = e.clientY - rect.top;
+                        }
+
+                        $body.bind(eventsMap[device].move, function(e){
+                            if(e.touches){
+                                e.preventDefault();
+                                point = {
+                                    x: e.touches[0].clientX,
+                                    y: e.touches[0].clientY
+                                };
+                            }else{
+                                point = {
+                                    x: e.clientX,
+                                    y: e.clientY
+                                };
+                            }
+
+                            position = {
+                                left: point.x - relative.x + 'px',
+                                top: point.y - relative.y + 'px'
+                            };
+                            
+                            element.css(position);
+                        });
+
+                        $body.bind(eventsMap[device].up, function(e){
+                            $body.off(eventsMap[device].move);
+                            $body.off(eventsMap[device].up);
+                        });
+                    }
+                }
+            };
+        }
+    ]);
 
     return app;
 });
