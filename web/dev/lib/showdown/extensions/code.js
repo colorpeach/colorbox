de9b8ce042262821622b4bc389c7615034520935
colorpeach <514571352@qq.com>
2015-01-09 17:21:00 +0800

    optimize article markdown preview render

--- web/dev/lib/showdown/extensions/code.js
+++ web/dev/lib/showdown/extensions/code.js
@@ -5,12 +5,91 @@
 //
 
 (function(){
+    var div = document.createElement('div');
+    div.style.height = '400px';
+    div.style.width = '400px';
+    div.style.position = 'fixed';
+    div.style.top = 0;
+    div.style.left = '-100%';
+
+    document.body.appendChild(div);
+
+    var cache = {
+        flow: {},
+        sequence: {}
+    };
+    var Diagram;
+    var flowchart;
 
     var code = function(converter) {
         return [
             { type: 'output', filter: function(source){
-
                 return source.replace(/<code class="(.+)">([\S\s]*?)<\/code>/g, function(match, type, code) {
+                    if(type === 'sequence'){
+                        if(!Diagram){
+                            require(['sequence-diagram'], function(d){ Diagram = d;});
+                        }else{
+                            var key = angular.toJson(code);
+                            if(key in cache.sequence){
+                                cache.sequence[key].store = true;
+                                return cache.sequence[key].content;
+                            }else{
+                                var diagram = Diagram.parse(code);
+                                div.innerHTML = '';
+                                diagram.drawSVG(div);
+                                //以文本为键，缓存绘制好的图像
+                                cache.sequence[key] = {
+                                    content: div.innerHTML,
+                                    store: true
+                                }
+                                return div.innerHTML;
+                            }
+                        }
+
+                            //删除不再需要的缓存
+                            for(var n in cache.sequence){
+                                if(!cache.sequence[n].store)
+                                    delete cache.sequence[n];
+                            }
+                    }else if(type === 'flow'){
+                        if(!flowchart){
+                            require(['flowchart'], function(f){ flowchart = f;});
+                        }else{
+                            var key = angular.toJson(code);
+                            if(key in cache.flow){
+                                cache.flow[key].store = true;
+                                return cache.flow[key].content;
+                            }else{
+                                var diagram = flowchart.parse(code);
+                                div.innerHTML = '';
+                                diagram.drawSVG(div);
+                                //以文本为键，缓存绘制好的图像
+                                cache.flow[key] = {
+                                    content: div.innerHTML,
+                                    store: true
+                                }
+                                return div.innerHTML;
+                            }
+                        }
+
+                        //删除不再需要的缓存
+                        for(var n in cache.flow){
+                            if(!cache.flow[n].store)
+                                delete cache.flow[n];
+                        }
+                    }else{
+                        if(typeof hljs !== 'undefined'){
+                            require(['highlight']);
+                        }else{
+
+                        }
+//                         require(['highlight'], function(highlight){
+//                             angular.forEach($code, function(n){
+//                                 hljs.highlightBlock(n);
+//                             });
+//                         });
+                    }
+
                     return match;
                 });
             }}
